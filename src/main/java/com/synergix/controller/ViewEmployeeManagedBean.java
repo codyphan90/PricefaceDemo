791862930f37fce476354b033a11216ee8a74473
phanvietanh <phanvietanh90@gmail.com>
2017-03-29 11:15:40 +0700

    create emp list + show on dataTable page

---
+++ src/main/java/com/synergix/controller/ViewEmployeeManagedBean.java
@@ -0,0 +1,39 @@
+package com.synergix.controller;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import com.synergix.training.model.Employee;
+
+@ManagedBean
+@SessionScoped
+public class ViewEmployeeManagedBean {
+	private List<Employee> employees = new ArrayList<Employee>();
+
+	public ViewEmployeeManagedBean() {
+		
+	}
+	
+	@PostConstruct
+	public void populcateEmployeeList(){
+		for (int i = 0; i <= 10; i++) {
+			Employee emp = new Employee();
+			emp.setEmployeeID(String.valueOf(i));
+			emp.setEmployeeName("Nguyen Van "+i);
+			this.employees.add(emp);
+		}
+	}
+
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+	
+
+}
---
+++ src/main/java/com/synergix/training/model/Employee.java
@@ -0,0 +1,20 @@
+package com.synergix.training.model;
+
+public class Employee {
+	private String employeeID;
+	private String employeeName;
+	public String getEmployeeID() {
+		return employeeID;
+	}
+	public void setEmployeeID(String employeeID) {
+		this.employeeID = employeeID;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	
+
+}
